24c9cde05f44dad9deed4b4f6073fd5afc102818
Artem Morozov <moroz.20000@gmail.com>
2021-05-16 11:47:49 +0300

    commit #2

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ const popupButton = document.querySelector(".popup__button");
 const popupInput = document.querySelector(".popup__answer");
 const popupTitle = document.querySelector(".popup__title");
 
-const questArr = [['1+1=','2*3=','3-1=','22+4=','3+1=','2+2=','21-2=','2+10=','23+10=','9/3='],['2','6','2','26','4','4','19','12','13','6']];
+const questArr = [
+                    [
+                        'У одного мужчины четыре сына, и у каждого из них есть родная сестра. Сколько у него детей?',
+                        'Полтора судака стоят полтора рубля. Сколько стоят три судака?',
+                        'Когда гусь стоит на одной ноге,он весит 7 кг. Сколько будет весить гусь,если встанет на две ноги?',
+                        'Бабушке Ани 70 лет, мама в 2 раза моложе бабушки, а Аня на 26 лет моложе мамы. Сколько лет Ане?',
+                        'Карандаш разделили на три части. Сколько сделали разрезов?',
+                        'Одно яйцо нужно варить 5 минут. Сколько времени потребуется,чтобы сварить 6 яиц?',
+                        'В автобусе едет 15 детей. Это составляет одну треть всех пассажиров. Сколько всего пассажиров едут в автобусе?',
+                        'Сын с отцом, да сын с отцом,да дедушка с внуком.Много ли их?',
+                        '5 рыбаков съели 5 карпов за 5 дней. Как вы думаете,а за сколько дней 15 рыбаков съедят 15 карпов?',
+                        'На столе лежит яблоко. Его разделили на 4 части.Сколько яблок лежит на столе?'
+                    ],
+                    [
+                        '5',
+                        '3',
+                        '7',
+                        '9',
+                        '2',
+                        '5',
+                        '45',
+                        '3',
+                        '5',
+                        '1'
+                    ]
+                ];
 
 let qAnda = 0;
 let point = 0;
@@ -37,6 +62,7 @@ function nextQuest(){
 }
 function validRaund(){
     if(raund===11){
+        popupButton.classList.add('popup__answer_hidden');
         popupInput.classList.add('popup__answer_hidden');
         popupButton.disabled = true;
         popupQuest.innerHTML = "Молодец ты набрал "+point;
@@ -60,6 +86,7 @@ function closePopup(){
 }
 
 buttonOpne.addEventListener('click',()=>{
+    popupButton.classList.remove('popup__answer_hidden');
     openPopup();
 })
 buttonClose.addEventListener('click',()=>{
